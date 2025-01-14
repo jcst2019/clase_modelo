419e9fce0595e35f9158d671b48bddd8be491e2e
jcst2019 <juansolar_4@hotmail.com>
2025-01-13 23:24:19 -0500

    Agregar la clase Alumno

---
+++ src/main/java/com/idat/clasemodelo/Alumno.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.idat.clasemodelo;
+
+/**
+ *
+ * @author Solaris
+ */
+public class Alumno {
+    
+    private int id;
+    private String nombre;
+    private int edad;
+    private String direccion;
+    private String telefono;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "id=" + id + ", nombre=" + nombre + ", edad=" + edad + ", direccion=" + direccion + ", telefono=" + telefono + '}';
+    } 
+}
